133dba2dde33e2419b5e8194d5261168ddea8500
razzaksr <razzaksr@gmail.com>
2022-10-31 11:09:46 +0530

    methods started

---
+++ SrinivasanBasic/Blocks.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SrinivasanBasic
+{
+    internal class Blocks
+    {
+        public float cmToInch(int centi)
+        {
+            return (float)(centi * 0.393);
+        }
+        public void dollarToRupees(int dollar)
+        {
+            Console.WriteLine(dollar * 79.8);
+        }
+    }
+}
--- SrinivasanBasic/Program.cs
+++ SrinivasanBasic/Program.cs
@@ -1,9 +1,10 @@
 ﻿using SrinivasanBasic;
 
-LoopStatements loop = new LoopStatements();
-//loop.demoWhile();
-//loop.demoDoWhile();
-//loop.demoFor();
-//loop.demoInfinite();
-//loop.demoNested();
-loop.demoPatterns();
+Blocks bk = new Blocks();
+bk.dollarToRupees(9000);
+bk.dollarToRupees(700);
+bk.dollarToRupees(0);
+bk.dollarToRupees(12);
+
+float term=bk.cmToInch(156);
+Console.WriteLine(term);
