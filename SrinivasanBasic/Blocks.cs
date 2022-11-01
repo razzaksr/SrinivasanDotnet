cd7c6cb7b693ecb16ab80077c06e7bf36d40119c
razzaksr <razzaksr@gmail.com>
2022-11-01 15:50:46 +0530

    Methods over static and non static

--- SrinivasanBasic/Blocks.cs
+++ SrinivasanBasic/Blocks.cs
@@ -8,6 +8,74 @@ namespace SrinivasanBasic
 {
     internal class Blocks
     {
+        public static int swaps(out int spring,out int jpa)
+        {
+            int calm = 90, palm=20;
+            spring = calm;
+            jpa = palm;
+            return 10000;
+        }
+        public int hello()
+        {
+            return 10;
+        }
+        public void just(ref int react,ref int axios)
+        {
+            Console.WriteLine("Recieved" + react + " " + axios);
+            react ^= axios;
+            axios ^= react;
+            react ^= axios;
+            Console.WriteLine("Changed" + react + " " + axios);
+        }
+        public void just(int one,int two)
+        {
+            Console.WriteLine("Recieved"+ one + " " +two);
+            one ^= two;
+            two ^= one;
+            one ^= two;
+            Console.WriteLine("Changed" + one + " " + two);
+        }
+        public int otp()
+        {
+            Random random = new Random();
+            String tmp = "";
+            for (int num=1;num<=6;num++)
+            {
+                tmp+=random.Next(10);
+            }
+            int pin = Convert.ToInt32(tmp);
+            //Console.WriteLine(pin);
+            return pin;
+        }
+        public void access()
+        {
+            int size=Convert.ToInt32(Console.ReadLine());
+            int limit = size * 2 - 1;
+            for (int row=size;row>=1;row--)
+            {
+                for (int space=size;space>row;space--)
+                {
+                    Console.Write(" ");
+                }
+                for (int data=1;data<=limit;data++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+                limit -= 2;
+            }
+        }
+        public void eric(int sizeA, int sizeB)
+        {
+            for (int axe=1; axe<=sizeA; axe++)
+            {
+                for (int bot=1;bot<=sizeB;bot++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
         public float cmToInch(int centi)
         {
             return (float)(centi * 0.393);
--- SrinivasanBasic/Program.cs
+++ SrinivasanBasic/Program.cs
@@ -8,3 +8,28 @@ bk.dollarToRupees(12);
 
 float term=bk.cmToInch(156);
 Console.WriteLine(term);
+
+//bk.eric(2, 3);
+//bk.access();
+
+//int ot=bk.otp();
+//Console.WriteLine(ot);
+
+Console.WriteLine(bk.otp());
+
+
+// call by value
+int cosmo = 45, delta = 90;
+Console.WriteLine(cosmo+" "+delta);
+bk.just(cosmo, delta);
+Console.WriteLine(cosmo + " " + delta+" @ main");
+
+// call by reference
+bk.just(ref cosmo, ref delta);
+Console.WriteLine(cosmo + " " + delta + " @ main");
+
+int j=bk.hello();
+Console.WriteLine(j);
+
+Blocks.swaps(out cosmo, out delta);
+Console.WriteLine("After out " + cosmo + " and " + delta);
