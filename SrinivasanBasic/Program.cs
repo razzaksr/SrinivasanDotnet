953da0d7d0391f029719b051ffca510a96a65da4
razzaksr <razzaksr@gmail.com>
2022-11-02 15:25:36 +0530

    Recursive methods

--- SrinivasanBasic/Program.cs
+++ SrinivasanBasic/Program.cs
@@ -1,26 +1,12 @@
 ﻿using SrinivasanBasic;
 
-Members mem = new Members();
-mem.max = 123;
-mem.actSome();
-mem.doSome();
+Recursive rec=new Recursive();
+//rec.grant();
 
-Members mem2 = new Members();
-mem2.max = 56666;
-mem2.actSome();
-mem2.doSome();
+rec.seekSome();
 
+//rec.retrospective();
 
-Builds.hai = "Razak Mohamed S";
-Builds.showSome();
+rec.range(1, 10);
 
-Builds build1 = new Builds();
-//build1.showSome();
-build1.viewSome();
-
-Builds.hai = "Zealous academy";
-
-Builds build2 = new Builds();
-
-build1.viewSome();
-build2.viewSome();
+rec.range(101, 200);
---
+++ SrinivasanBasic/Recursive.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SrinivasanBasic
+{
+    internal class Recursive
+    {
+        public void seekSome()
+        {
+            Console.WriteLine("Seeking Authority");
+            grant();
+        }
+        public void grant()
+        {
+            Console.WriteLine("Granting authority from HR to Team Lead");
+        }
+        public void retrospective()
+        {
+            Console.WriteLine("Scrum meeting happens at end of the sprint");
+            retrospective();
+        }
+        public void range(int start,int end)
+        {
+            if (start <= end)
+            {
+                Console.WriteLine("Welcome to the hell "+start);
+                start++;
+                range(start, end);
+            }
+            else
+                return;
+            
+        }
+    }
+}
