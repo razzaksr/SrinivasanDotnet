b9629a5d491bee18c8572dc85e38cf9fb0a9b687
razzaksr <razzaksr@gmail.com>
2022-11-04 15:37:05 +0530

    Operator overloading and compile time poly done

--- SrinivasanOOP/Program.cs
+++ SrinivasanOOP/Program.cs
@@ -1,15 +1,23 @@
 ﻿using SrinivasanOOP;
 
-Event eve1 = new Event();
-eve1.eveId = 1; eve1.eveName = "Hackathon"; eve1.eveDate = DateTime.Parse("2022-10-10");
-eve1.eveParts = new string[] {"Annamalai","Manoj","Rasheedha" };
-eve1.eveWinner = "Rasheedha";
+Bank b1 = new Bank();b1.accBal = 8900.4;b1.accNum = 9876567876545;
+Console.WriteLine(b1 - 5000);
+Console.WriteLine(b1);
 
-//Console.WriteLine(eve1.eveWinner+" "+eve1.eveId+" "+eve1.eveName+" "+eve1.eveParts+" "+eve1.eveDate);
-Console.WriteLine(eve1);
 
-Event eve2 = new Event(2100,"PPT",DateTime.Parse("2022-11-01"));
-Console.WriteLine(eve2.eveWinner + " " + eve2.eveId + " " + eve2.eveName + " " + eve2.eveParts + " " + eve2.eveDate);
-Event eve3 = new Event(2100, "PPT", new string[] { "Annamalai", "Manoj", "Rasheedha" }, DateTime.Parse("2022-11-01"),"Manoj");
-Console.WriteLine(eve3.eveWinner + " " + eve3.eveId + " " + eve3.eveName + " " + eve3.eveParts + " " + eve3.eveDate);
+/*
+Wind w1 = new Wind(56,9.8);
+Wind w2 = new Wind();
+w2.macro = 78;w2.micro = 89.5;
+Console.WriteLine(w1);
+Console.WriteLine(w2);
 
+int j = w1 + w2;
+Console.WriteLine(w1);
+Console.WriteLine(w2);
+
+Console.WriteLine(w1%w2);
+
+// : :: => 
+
+*/
---
+++ SrinivasanOOP/Wind.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SrinivasanOOP
+{
+    internal class Bank
+    {
+        public static String operator-(Bank obj,int amount)
+        {
+            if (amount<=obj.accBal)
+            {
+                obj.accBal -= amount;
+                return amount + " has been withdrawn successfully";
+            }
+            else
+            {
+                return "Insufficient balance";
+            }
+        }
+        public long accNum { set; get; }
+        public double accBal { set; get; }
+        public override string ToString()
+        {
+            return "Number is "+accNum+" balance is "+accBal;
+        }
+    }
+    internal class Wind
+    {
+        public static String operator%(Wind obj1, Wind obj2)
+        {
+            if (obj1.macro>=obj2.macro)
+            {
+                return obj1.ToString()+" is greatest object";
+            }
+            else
+            {
+                return obj2.ToString() + " is greatest object";
+            }
+        }
+        public static int operator +(Wind obj1,Wind obj2)
+        {
+            obj1.micro *= obj2.micro;
+            obj2.micro = obj1.micro / obj2.micro;
+            obj1.micro /= obj2.micro;
+            
+            return 0;
+        }
+        public Wind() { }
+        public Wind(int macro, double micro)
+        {
+            this.macro = macro;
+            this.micro = micro;
+        }
+        public override string ToString()
+        {
+            return macro+" "+micro;
+        }
+        public int macro { set; get; }
+        public double micro { set; get; }
+    }
+}
