4f400960312e3a7800e25a04d68cfbcbe31709ec
razzaksr <razzaksr@gmail.com>
2022-11-04 13:22:47 +0530

    Encapsulation done

---
+++ SrinivasanOOP/Event.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SrinivasanOOP
+{
+    internal class Event
+    {
+        public override string ToString()
+        {
+            return eveName + " " + eveId + " " + eveDate + " " + eveParts + " " + eveWinner + "\n";
+        }
+        // default constructor
+        public Event() { Console.WriteLine("Hai there!"); }
+        //parameterized constructor
+        public Event(int eveId, String eveName, DateTime eveDate)
+        {
+            this.eveId = eveId;
+            this.eveName = eveName;
+            this.eveDate = eveDate;
+        }
+        //parameterized constructor
+        public Event(int eveId,String eveName, String[] eveParts,DateTime eveDate,String eveWinner) 
+        {
+            this.eveId = eveId;
+            this.eveName = eveName;
+            this.eveParts = eveParts;   
+            this.eveDate = eveDate;
+            this.eveWinner = eveWinner;
+        }
+        public int eveId { set; get; }
+        public String eveName { set; get; }
+        public String[] eveParts { set; get; }
+        public DateTime eveDate { set; get; }
+        public String eveWinner { set; get; }
+        /*
+        int eveId;
+        public void setEveId(int eveId)
+        {
+            this.eveId = eveId;
+        }
+        public int getEveId() { return eveId; }
+        string eveName;
+        DateAndTime eveDate;
+        String[] eveParts = new String[10];
+        String eveWinner;
+        */
+    }
+}
---
+++ SrinivasanOOP/Program.cs
@@ -0,0 +1,15 @@
+﻿using SrinivasanOOP;
+
+Event eve1 = new Event();
+eve1.eveId = 1; eve1.eveName = "Hackathon"; eve1.eveDate = DateTime.Parse("2022-10-10");
+eve1.eveParts = new string[] {"Annamalai","Manoj","Rasheedha" };
+eve1.eveWinner = "Rasheedha";
+
+//Console.WriteLine(eve1.eveWinner+" "+eve1.eveId+" "+eve1.eveName+" "+eve1.eveParts+" "+eve1.eveDate);
+Console.WriteLine(eve1);
+
+Event eve2 = new Event(2100,"PPT",DateTime.Parse("2022-11-01"));
+Console.WriteLine(eve2.eveWinner + " " + eve2.eveId + " " + eve2.eveName + " " + eve2.eveParts + " " + eve2.eveDate);
+Event eve3 = new Event(2100, "PPT", new string[] { "Annamalai", "Manoj", "Rasheedha" }, DateTime.Parse("2022-11-01"),"Manoj");
+Console.WriteLine(eve3.eveWinner + " " + eve3.eveId + " " + eve3.eveName + " " + eve3.eveParts + " " + eve3.eveDate);
+
